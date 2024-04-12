ef588caab3f2307176051e823370a75a23feb00f
Javier L. Avrudsky <javier.avrudsky@split.io>
2024-04-12 10:31:51 -0300

    Release 0.3.0-rc4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SplitRum",
-            url: "https://cdn.split.io/rum-agent-ios/SplitRumAgent_0.3.0-rc3.zip",
-            checksum: "263af652dca30dcb59b22ad90274e44d11161e196e508e1c676c4710d9e91650"
+            url: "https://cdn.split.io/rum-agent-ios/SplitRumAgent_0.3.0-rc4.zip",
+            checksum: "014c613682de1cba5211fa430ad8f0c7add1853c95bd3451142fadadd5d913d5"
         ),
     ]
 )
